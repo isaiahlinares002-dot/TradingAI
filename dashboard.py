1c411ef30251a726e56f96de3d6b282c9befa92a
isaiahlinares002-dot <isaiahlinares002@gmail.com>
2026-03-29 22:03:25 -0400

    Update dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Marcus.AI")
+
+st.title("🤖 Marcus.AI is LIVE")
+
+st.write("If you see this, your app is working.")
+
+st.button("Test Button")
